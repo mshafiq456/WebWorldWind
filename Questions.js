edeb093c42519b1c680c202a98d731773bcdc930
mshafiq456 <mshafiq456@gmail.com>
2015-08-10 19:37:40 -0700

    Adding All the files
    These are all the files for Earthquake app

---
+++ Questions.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Shafiq on 8/1/2015.
+ *
+ * This File ONLY contains QUESTIONS
+ *
+ * 1-----------------------------------------------------
+ *  How do I enable Webstorm to autodetect errors instead of letting it do something and skipping what is not there
+ *      for example
+ *          when we do for a header:
+ *              document.getElementByName('h1')[0]
+ *              this don't do anything; Doens't even tell that the error is the fact that it should be
+ *              document.getElementByTagName('h1')[0] instead
+ *
+ *
+ *
+ *
+ *
+ * 2----------------------------------------------------
+ *
+ *  It doens't tell me when i type something wrong
+ *      for example
+ *          <h1 alight="centered" ...>...</h1>
+ *          in this case "centered" is not defined but it doens't detect it.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
